e99f3e5a319713c8acd16eaeade92d60829db084
Heneryljl <214216@clvsc.tyc.edu.tw>
2025-02-13 14:33:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,43 +1,42 @@
-import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
 
 app = Flask(__name__)
 
-# 從環境變數中讀取 Channel Secret 和 Channel Access Token
-CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
-CHANNEL_ACCESS_TOKEN = os.environ.get('CHANNEL_ACCESS_TOKEN')
+# 設定 LINE Channel Access Token 和 Secret
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 line_handler = WebhookHandler(CHANNEL_SECRET)
+@app.route('/')
+def home():
+    return "LINE BOT 首頁"
 
-@app.route("/callback", methods=['POST'])
+@app.route("/callback", methods=["POST"])
 def callback():
-    # 獲取 Line 發送過來的簽名
-    signature = request.headers['X-Line-Signature']
-
-    # 獲取請求的正文內容
+    signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
 
     try:
-        # 驗證簽名
         line_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return "OK"
 
-# 監聽文本消息事件
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 回傳與收到的訊息相同的內容
+    user_message = event.message.text
+    reply_message = f"你說了：{user_message}"
+    
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=reply_message)
     )
 
 if __name__ == "__main__":
-    # 使用 Vercel 預設端口
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(port=8000)
